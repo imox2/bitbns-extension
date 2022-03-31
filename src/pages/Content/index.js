ae11ee7f6fc49051d3f122788aa477daba9ed045
Talha <talhaanwar.anwar@gmail.com>
2022-03-31 21:55:26 +0530

    correct not able to get html from page

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -4,3 +4,34 @@ console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
+// function inContent1() {
+//     const el = document.createElement('div');
+//     el.style.cssText = 'position:fixed; top:0; left:0; right:0; background:red';
+//     el.textContent = 'DIV';
+//     document.body.appendChild(el);
+//   }
+
+function getDataFromDOM(selector) {
+    var domNode = document.querySelector('.overflow-auto');
+    console.log("document.all[0]:", document.all[0]);
+    console.log("document:", document)
+    console.log("domNode:", domNode)
+    if (domNode) {
+        return domNode;
+    }
+    return undefined;
+}
+
+async function getHTML() {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: getDataFromDOM, // see inContent1 in ManifestV2 example above
+    });
+}
+
+// function getHTML() {
+//     return getDataFromDOM();
+// }
+
+export { getHTML };
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
+import { getHTML } from '../Content/index';
 import './Popup.css';
 
+window.chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log('message1');
+  // if (message.type === "OYO_PLUGIN_EVALUATED_CONFIG") {
+  //   this.setState({
+  //     configData: message.configData
+  //   });
+  // }
+});
+
 const Popup = () => {
+  const readHtml = () => {
+    // read html from current page
+    console.log("html:", getHTML());
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +34,7 @@ const Popup = () => {
         >
           Learn React!
         </a>
+        <button onClick={readHtml}>click me</button>
       </header>
     </div>
   );
