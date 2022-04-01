cd59ef1bbc01bc90240fca93b3b174f2f4126932
durgesh_kumar_gupta <durgeshkumar.gupta@appzlogic.com>
2022-04-02 01:12:31 +0530

    sell and buy filter range created

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -3,6 +3,7 @@ import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
 import { getHTML } from '../Content/index';
 import './Popup.css';
+import moment from 'moment'
 
 window.chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log('message1');
@@ -17,6 +18,11 @@ window.chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 const Popup = () => {
   let data;
   const [records, setRecords] = React.useState([]);
+  const [fromattedData, setFormattedData] = React.useState([]);
+  const [showTableData, setShowTableData] = React.useState([]);
+
+  const [fDate, setFDate] = React.useState(new Date());
+  const [tDate, setTDate] = React.useState(new Date())
   React.useEffect(() => {
     console.log("records in:", records);
   }, [records])
@@ -60,16 +66,74 @@ const Popup = () => {
     console.log("records:", records)
   }
 
+  const showData = (type) => {
+    setShowTableData([])
+    manageData()
+    let data = fromattedData.filter(item => fDate <= item.date && tDate >= item.date)
+    if (type == 'buy') {
+      const buyData = data.filter(item => item.type == 'Sell')
+      setShowTableData(buyData)
+    } else if (type == 'sell') {
+      const sellData = data.filter(item => item.type == 'Buy')
+      setShowTableData(sellData)
+    }
+    console.log('data: ', showTableData)
+
+  }
+
+  const manageData = () => {
+    let data = []
+    records.map(item => {
+      let type = item.description.split(" ");
+      let cType = type[1];
+
+      let date = item.date;
+      let dateArr = date.split(",")
+      let newDate = dateArr[1].trim()
+
+      let mDate = moment(newDate).format('YYYY-MM-DD')
+      data.push({ type: cType, date: mDate, amount: item.amount, fee: item.fee })
+    })
+
+    setFormattedData(data)
+  }
+
+
   return (
     <div className="App" style={{ background: "white" }}>
 
       <button onClick={() => { readHtml(); }}>click me</button>
       <h2>Matic Data</h2>
-      <ul >
+      {/* <ul >
         {records.length > 0 ? records.map((record, index) =>
-          <li>{JSON.stringify(record)}</li>
+          <li>{record.date}</li>
         ) : ''}
-      </ul>
+      </ul> */}
+
+      <div>
+        <div>
+          <input type="date" value={fDate} onChange={(e) => { setFDate(e.target.value) }} />
+        </div>
+        <div>
+          <input type="date" value={tDate} onChange={(e) => { setTDate(e.target.value) }} />
+        </div>
+        <button onClick={() => { showData('buy') }}>Buy</button>
+        <button onClick={() => { showData('sell') }}>Sell</button>
+        <table>
+          <tr>
+            <th>Amount</th>
+            <th>Fee</th>
+            <th>Date</th>
+          </tr>
+          {showTableData.lenght > 0 && showTableData.map((item, i) => {
+            <tr key={i}>
+              <td>{item.amount}</td>
+              <td>{item.fee}</td>
+              <td>{item.date}</td>
+            </tr>
+          })}
+        </table>
+      </div>
     </div>
   );
 };
